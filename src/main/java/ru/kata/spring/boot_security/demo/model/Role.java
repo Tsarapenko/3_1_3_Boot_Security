2182f3849484194a1050a2d19e6513bfe7a72da9
Tsarapenko <120114047+Tsarapenko@users.noreply.github.com>
2023-05-10 13:23:41 +0300

    First commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -0,0 +1,98 @@
+package ru.kata.spring.boot_security.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roleList")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id.equals(role.id) && name.equals(role.name) && users.equals(role.users);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, users);
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", users=" + users +
+                '}';
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+
+    public String getNameRole(){
+        return name.substring("ROLE_".length());
+    }
+
+
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,19 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+@Component
+public interface UserService {
+
+    User findUserById(Long id);
+    List<User> allUsers();
+     boolean saveUser(User user);
+    void deleteUser(Long id);
+    void update (User user, Long id);
+
+    User findByUsername (String username);
+}
